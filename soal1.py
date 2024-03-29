16c57f6b87185024b70819dbd70e0d9d668b055a
FiraaAisah <firaaisah64@gmail.com>
2024-03-29 23:07:26 +0800

    first commit

---
+++ soal1.py
@@ -0,0 +1,7 @@
+for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print(j + (5 - i), end=" ")
+    print()
+
+
+
---
+++ soal4.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    a = b
+    for j in range (5):
+        print (a, end=" ")
+        a += c
+    b += 1
+    c += 1
+    print()
